8c8696b83e8c9bd12210d116f8be144f59f392c8
Torin Scholes <torinscholes@gmail.com>
2020-08-15 20:55:19 -0600

    Started project and added basic global exception handling.

--- GoalTracker/App.xaml.cs
+++ GoalTracker/App.xaml.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace GoalTracker {
    /// <summary>
    /// Interaction logic for App.xaml
    /// </summary>
    public partial class App : Application {
+      private void ApplicationUnhandledException(Object sender, DispatcherUnhandledExceptionEventArgs e) {
+         MessageBox.Show("An unhandled exception just occured: " + e.Exception.Message, "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+         e.Handled = true;
+      }
    }
 }
