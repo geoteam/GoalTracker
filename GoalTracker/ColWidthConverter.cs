7a4d6ed28dcec020aa7eb5225bf896198e4c3037
Torin Scholes <torinscholes@gmail.com>
2020-08-19 21:14:27 -0600

    Worked dynamic resizing into the grid for the main window.

---
+++ GoalTracker/ColWidthConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GoalTracker {
+   class ColWidthConverter : IMultiValueConverter {
+      public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+         return ((Double)values[0] * (Double)values[1]) / 100.0;
+      }
+
+      public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+         throw new NotImplementedException();
+      }
+   }
+}
